08d96e8534e5595f9b72819b7d3bd797d447ab98
Dbachelor <ojiodujoachim95@gmail.com>
2024-12-03 18:48:41 +0100

    added cors waiver

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const multer = require('multer');
+const cors = require('cors');
 // const fs = require('fs').promises;
 // const path = require('path');
 // const { analyzeAccessibility } = require('./services/htmlAnalyzer');
 const { analyzeFile } = require('./controllers/htmlAnalyzerController');
 
 
+
 const app = express();
 const uploads = multer({ dest: 'uploads/' });
 
-
+let corsOptions = {
+    origin: [ 'http://localhost:3001', 'http://localhost:3000' ]
+};
 app.use(express.json());
 
 
-app.post('/upload', uploads.single('htmlFile'), analyzeFile);
+app.post('/upload', cors(corsOptions), uploads.single('htmlFile'), analyzeFile);
 
 
 app.listen(5000, () => console.log('running'));
